c714dd5a277371fc037d8492d1cf2b9dafd64ac6
Jonathan Nuno <jnuno.423@gmail.com>
2020-09-17 12:04:17 -0400

    updated code

--- jsone-files.py
+++ jsone-files.py
@@ -5,25 +5,25 @@ import json
 users = []
 
 #READ THE FILE
-with open("users.json") as file_object:
-    users = json.load(file_object)
+# with open("users.json") as file_object:
+#     users = json.load(file_object)
 
-print(users[0])
+# print(users)
 
-# while True:
-#     name = input("Enter name: ")
-#     age = int(input("Enter age: "))
+while True:
+    name = input("Enter name: ")
+    age = int(input("Enter age: "))
 
-#     user = {"name" : name, "age" : age}
-#     users.append(user)
-# # Create a simple dictionary to represent a user
-# # user_dictionary = {"name" : "John Doe", "age" : 45}
+    user = {"name" : name, "age" : age}
+    users.append(user)
+# Create a simple dictionary to represent a user
+# user_dictionary = {"name" : "John Doe", "age" : 45}
 
-# # write the dictionary to json file
-#     with open("users.json", "w") as file_object:
-#         #json.dump(DataYouWantToWriteToJson, FileObjecToUse)
-#         json.dump(users, file_object)
+# write the dictionary to json file
+    with open("users.json", "w") as file_object:
+        #json.dump(DataYouWantToWriteToJson, FileObjecToUse)
+        json.dump(users, file_object)
 
-#     choice = input("Any key to continue or q to quit: ")
-#     if choice == "q":
-#         break
+    choice = input("Any key to continue or q to quit: ")
+    if choice == "q":
+        break
