f81d3afc6ef690e7e5b437e21b690b4c0be9db94
Jonathan Nuno <jnuno.423@gmail.com>
2020-09-17 11:27:17 -0400

    python json lesson

---
+++ jsone-files.py
@@ -0,0 +1,29 @@
+
+import json
+
+
+users = []
+
+#READ THE FILE
+with open("users.json") as file_object:
+    users = json.load(file_object)
+
+print(users[0])
+
+# while True:
+#     name = input("Enter name: ")
+#     age = int(input("Enter age: "))
+
+#     user = {"name" : name, "age" : age}
+#     users.append(user)
+# # Create a simple dictionary to represent a user
+# # user_dictionary = {"name" : "John Doe", "age" : 45}
+
+# # write the dictionary to json file
+#     with open("users.json", "w") as file_object:
+#         #json.dump(DataYouWantToWriteToJson, FileObjecToUse)
+#         json.dump(users, file_object)
+
+#     choice = input("Any key to continue or q to quit: ")
+#     if choice == "q":
+#         break
